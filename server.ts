9e8ed2ba1337894aba0cfccf039bcee9ef948284
djacquin <danielcamilo853@gmail.com>
2022-01-13 00:43:44 -0500

    the connection to the database is created

--- server.ts
+++ server.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/connection';
 dotenv.config();
 
 const server = express();
 const PORT = process.env.PORT || 3000;
+const CONNECTION_URL = process.env.CONNECT_URL;
 server.use(express.json());
 
-
-server.listen(PORT, () => {
-    console.log(`server is running on port: ${PORT}`)
-})
+const start = async() => {
+    try {
+        await connectDB(CONNECTION_URL);
+        server.listen(PORT, () => {
+            console.log(`server is running on port: ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
